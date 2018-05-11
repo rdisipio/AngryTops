57cc51282b1a2c9a4253ac7e04f74cbfa659f639
Riccardo Di Sipio <riccardo.di.sipio@cern.ch>
2018-05-11 09:42:23 -0400

    commit

---
+++ features.py
@@ -0,0 +1,27 @@
+header = [
+  "runNumber", "eventNumber", "weight",
+  "j1_px", "j1_py", "j1_pz", "j1_E", "j1_m", "j1_mv2c10",
+  "j2_px", "j2_py", "j2_pz", "j2_E", "j2_m", "j2_mv2c10",
+  "j3_px", "j3_py", "j3_pz", "j3_E", "j3_m", "j3_mv2c10",
+  "j4_px", "j4_py", "j4_pz", "j4_E", "j4_m", "j4_mv2c10",
+  "j5_px", "j5_py", "j5_pz", "j5_E", "j5_m", "j5_mv2c10",
+  "j6_px", "j6_py", "j6_pz", "j6_E", "j6_m", "j6_mv2c10",
+  "j7_px", "j7_py", "j7_pz", "j7_E", "j7_m", "j7_mv2c10",
+  "t_px",    "t_py",    "t_pz",    "t_E",    "t_m", 
+  "tbar_px", "tbar_py", "tbar_pz", "tbar_E", "tbar_m", 
+]
+
+input_features = [
+  "j1_px", "j1_py", "j1_pz", "j1_E", "j1_m", "j1_mv2c10",
+  "j2_px", "j2_py", "j2_pz", "j2_E", "j2_m", "j2_mv2c10",
+  "j3_px", "j3_py", "j3_pz", "j3_E", "j3_m", "j3_mv2c10",
+  "j4_px", "j4_py", "j4_pz", "j4_E", "j4_m", "j4_mv2c10",
+  "j5_px", "j5_py", "j5_pz", "j5_E", "j5_m", "j5_mv2c10",
+  "j6_px", "j6_py", "j6_pz", "j6_E", "j6_m", "j6_mv2c10",
+  "j7_px", "j7_py", "j7_pz", "j7_E", "j7_m", "j7_mv2c10",
+]
+
+target_features = [
+  "t_px",    "t_py",    "t_pz",    "t_E",    "t_m",
+  "tbar_px", "tbar_py", "tbar_pz", "tbar_E", "tbar_m",
+]
