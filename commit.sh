502ee009601f575169fbfa424970ebc48fa7f437
Riccardo Di Sipio <riccardo.di.sipio@cern.ch>
2019-04-18 15:54:22 -0400

    commit

--- commit.sh
+++ commit.sh
@@ -2,4 +2,5 @@
 msg="commit"
 [ ! -z $1 ] && msg=$1
 
-git commit -a -m "$msg" ; git push origin master
+git commit -a -m "$msg"
+git push origin master
---
+++ setup_env.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+[ ! -d csv ] && mkdir -p csv
+[ ! -d filelists ] && mkdir -p filelists
