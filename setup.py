4af35204a5923f11a170672702fca1434a53334d
Riccardo Di Sipio <riccardo.di.sipio@cern.ch>
2019-08-26 11:50:45 -0400

    make pkg

---
+++ setup.py
@@ -0,0 +1,47 @@
+"""
+AngryTops
+"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open( 'AngryTops/__init__.py' ) as f:
+    version = f.readlines()[-1].split()[-1].strip("\"'")
+
+setup(
+	name='AngryTops',
+	version=version,
+	packages=['AngryTops'],
+	url='https://github.com/rdisipio/AngryTops',
+	keywords=[
+		'top quark',
+		'kinematics',
+		'reconstruction'
+		],
+	license='LICENSE',
+	description='Probabilistic kinematics reconstruction of top quark decay chain',
+	classifiers=[
+        	'License :: OSI Approved :: MIT License',
+        	'Operating System :: OS Independent',
+        	'Development Status :: 3 - Alpha',
+        	'Topic :: Scientific/Engineering :: Mathematics',
+        	'Topic :: Scientific/Engineering :: Physics',
+        	'Intended Audience :: Science/Research',
+        	'Programming Language :: Python'
+    	],
+	install_requires=requirements,
+	scripts=[
+		'scripts/check_nan.py',
+		'scripts/fit.py',
+		'scripts/histograms.py',
+		'scripts/launch_gpu_job.sh',
+		'scripts/root2csv.py',
+		'scripts/setup_env.sh',
+		]
+)
+
