dda9539f57672707fcba4d1131f9163526e24e6a
Riccardo Di Sipio <riccardo.di.sipio@cern.ch>
2019-08-26 12:26:23 -0400

    make pkg

--- scripts/delphes2csv.py
+++ scripts/delphes2csv.py
@@ -27,7 +27,7 @@ if len(sys.argv) > 3:
 
 # Maximum number of entries
 n_evt_max = -1
-#if len(sys.argv) > 2: n_evt_max = int( sys.argv[2] )
+# if len(sys.argv) > 2: n_evt_max = int( sys.argv[2] )
 
 ###############################
 # BUILDING OUTPUTFILE
@@ -199,167 +199,43 @@ for ientry in range(n_entries):
         print("Invalid t_lep values, P_z = {0}, M = {1}".format(
             t_lep.Pz(), t_lep.M()))
         continue
-    # if W_had.Pt() < 20:
-    #     print("Invalid W_had.pt: {}".format(W_had.Pt()))
-    #     continue
-    # if W_lep.Pt() < 20:
-    #     print("Invalid W_lep.pt: {}".format(W_lep.Pt()))
-    #     continue
-    # if b_had.Pt() < 20:
-    #     print("Invalid b_had.pt: {}".format(b_had.Pt()))
-    #     continue
-    # if b_lep.Pt() < 20:
-    #     print("Invalid b_lep.pt: {}".format(b_lep.Pt()))
-    #     continue
-    # # if np.abs(W_had.Eta()) > 2.5:
-    # #     print("Invalid W_had.eta: {}".format(W_had.Eta()))
-    # #     continue
-    # # if np.abs(W_lep.Eta()) > 2.5:
-    # #     print("Invalid W_lep.eta: {}".format(W_lep.Eta()))
-    # #     continue
-    # if np.abs(b_had.Eta()) > 2.5:
-    #     print("Invalid b_had.eta: {}".format(b_had.Eta()))
-    #     continue
-    # if np.abs(b_lep.Eta()) > 2.5:
-    #     print("Invalid b_lep.eta: {}".format(b_lep.Eta()))
-    #     continue
 
     ##############################################################
     # DERIVED EVENT-WISE QUANTITES
     # Sum of all Pt's
     H_t = tree.GetLeaf("ScalarHT.HT").GetValue(0)
-    # DeltaPhi( let, closest b) = min DeltaPhi( lep, biets), and mass(l,b)
-    if len(bjets) > 1:
-        closest_b = np.argmin([lep.Phi() - bjets[i].Phi()
-                               for i in range(len(bjets))])
-        DeltaPhi = lep.Phi() - bjets[closest_b].Phi()
-        InvPart = bjets[closest_b] + lep
-        InvMass = InvPart.M()
-    elif len(bjets) == 1:
-        closest_b = 0
-        DeltaPhi = lep.Phi() - bjets[0].Phi()
-        InvPart = bjets[0] + lep
-        InvMass = InvPart.M()
-    else:
-        closest_b = -1
-        DeltaPhi = -1
-        InvMass = -1
 
     vertex_n = tree.GetLeaf("Vertex_size").GetValue(0)
 
     ##############################################################
-    # Augment Data By Rotating 5 Different Ways
-    n_good += 1
-    phi = 0
-    flip_eta = False
-    # Set the phi angle of the lepton to zero
-    if len(sys.argv) > 4 and sys.argv[4] == 'aa':
-        phi = -1 * lep.Phi()
-    if len(sys.argv) > 5 and sys.argv[5] == 'bb':
-        flip_eta = True
-    print("Writing new row to csv file")
-    for i in range(n_data_aug):
-        # make event wrapper
-        lep_aug, jets_aug, met_phi_aug = RotateEvent(lep, jets, met_phi, phi)
-        lep_flipped, jets_flipped = FlipEta(lep_aug, jets_aug)
-        sjets0, target_W_had, target_b_had, target_t_had, target_W_lep, target_b_lep, target_t_lep = MakeInput(
-            jets_aug, W_had, b_had, t_had, W_lep, b_lep, t_lep)
-        sjets1, _, _, _, _, _, _ = MakeInput(
-            jets_flipped, W_had, b_had, t_had, W_lep, b_lep, t_lep)
-
-    # write out
-        csvwriter.writerow((
-            "%i" % runNumber, "%i" % eventNumber, "%.5f" % weight, "%i" % jets_n, "%i" % bjets_n,
-            "%.5f" % lep_aug.Px(),     "%.5f" % lep_aug.Py(),     "%.5f" % lep_aug.Pz(
-            ),     "%.5f" % lep_aug.E(),      "%.5f" % met_met,      "%.5f" % met_phi_aug,
-            "%.5f" % sjets0[0][0],  "%.5f" % sjets0[0][1],  "%.5f" % sjets0[0][
-                2],  "%.5f" % sjets0[0][3],  "%.5f" % sjets0[0][4],  "%.5f" % sjets0[0][5],
-            "%.5f" % sjets0[1][0],  "%.5f" % sjets0[1][1],  "%.5f" % sjets0[1][
-                2],  "%.5f" % sjets0[1][3],  "%.5f" % sjets0[1][4],  "%.5f" % sjets0[1][5],
-            "%.5f" % sjets0[2][0],  "%.5f" % sjets0[2][1],  "%.5f" % sjets0[2][
-                2],  "%.5f" % sjets0[2][3],  "%.5f" % sjets0[2][4],  "%.5f" % sjets0[2][5],
-            "%.5f" % sjets0[3][0],  "%.5f" % sjets0[3][1],  "%.5f" % sjets0[3][
-                2],  "%.5f" % sjets0[3][3],  "%.5f" % sjets0[3][4],  "%.5f" % sjets0[3][5],
-            "%.5f" % sjets0[4][0],  "%.5f" % sjets0[4][1],  "%.5f" % sjets0[4][
-                2],  "%.5f" % sjets0[4][3],  "%.5f" % sjets0[4][4],  "%.5f" % sjets0[4][5],
-            "%.5f" % target_W_had[0], "%.5f" % target_W_had[1], "%.5f" % target_W_had[
-                2], "%.5f" % target_W_had[3], "%.5f" % target_W_had[4],
-            "%.5f" % target_W_lep[0], "%.5f" % target_W_lep[1], "%.5f" % target_W_lep[
-                2], "%.5f" % target_W_lep[3], "%.5f" % target_W_lep[4],
-            "%.5f" % target_b_had[0], "%.5f" % target_b_had[1], "%.5f" % target_b_had[
-                2], "%.5f" % target_b_had[3], "%.5f" % target_b_had[4],
-            "%.5f" % target_b_lep[0], "%.5f" % target_b_lep[1], "%.5f" % target_b_lep[
-                2], "%.5f" % target_b_lep[3], "%.5f" % target_b_lep[4],
-            "%.5f" % target_t_had[0], "%.5f" % target_t_had[1], "%.5f" % target_t_had[
-                2], "%.5f" % target_t_had[3], "%.5f" % target_t_had[4],
-            "%.5f" % target_t_lep[0], "%.5f" % target_t_lep[1], "%.5f" % target_t_lep[
-                2], "%.5f" % target_t_lep[3], "%.5f" % target_t_lep[4],
-            "%.5f" % lep_aug.Pt(),     "%.5f" % lep_aug.Eta(),     "%.5f" % lep_aug.Phi(),
-            "%.5f" % sjets0[0][6],  "%.5f" % sjets0[0][7],  "%.5f" % sjets0[0][8],
-            "%.5f" % sjets0[1][6],  "%.5f" % sjets0[1][7],  "%.5f" % sjets0[1][8],
-            "%.5f" % sjets0[2][6],  "%.5f" % sjets0[2][7],  "%.5f" % sjets0[2][8],
-            "%.5f" % sjets0[3][6],  "%.5f" % sjets0[3][7],  "%.5f" % sjets0[3][8],
-            "%.5f" % sjets0[4][6],  "%.5f" % sjets0[4][7],  "%.5f" % sjets0[4][8],
-            "%.5f" % target_W_had[5], "%.5f" % target_W_had[6], "%.5f" % target_W_had[7],
-            "%.5f" % target_W_lep[5], "%.5f" % target_W_lep[6], "%.5f" % target_W_lep[7],
-            "%.5f" % target_b_had[5], "%.5f" % target_b_had[6], "%.5f" % target_b_had[7],
-            "%.5f" % target_b_lep[5], "%.5f" % target_b_lep[6], "%.5f" % target_b_lep[7],
-            "%.5f" % target_t_had[5], "%.5f" % target_t_had[6], "%.5f" % target_t_had[7],
-            "%.5f" % target_t_lep[5], "%.5f" % target_t_lep[6], "%.5f" % target_t_lep[7],
-            "%.5f" % H_t, "%i" % closest_b, "%.5f" % DeltaPhi, "%.5f" % InvMass
-        ))
-
-        if flip_eta:
-            csvwriter.writerow((
-                "%i" % runNumber, "%i" % eventNumber, "%.5f" % weight, "%i" % jets_n, "%i" % bjets_n,
-                "%.5f" % lep_flipped.Px(),     "%.5f" % lep_flipped.Py(
-                ),     "%.5f" % lep_flipped.Pz(),     "%.5f" % lep_flipped.E(),
-                "%.5f" % met_met,      "%.5f" % met_phi_aug,
-                "%.5f" % sjets1[0][0],  "%.5f" % sjets1[0][1],  "%.5f" % sjets1[0][
-                    2],  "%.5f" % sjets1[0][3],  "%.5f" % sjets1[0][4],  "%.5f" % sjets1[0][5],
-                "%.5f" % sjets1[1][0],  "%.5f" % sjets1[1][1],  "%.5f" % sjets1[1][
-                    2],  "%.5f" % sjets1[1][3],  "%.5f" % sjets1[1][4],  "%.5f" % sjets1[1][5],
-                "%.5f" % sjets1[2][0],  "%.5f" % sjets1[2][1],  "%.5f" % sjets1[2][
-                    2],  "%.5f" % sjets1[2][3],  "%.5f" % sjets1[2][4],  "%.5f" % sjets1[2][5],
-                "%.5f" % sjets1[3][0],  "%.5f" % sjets1[3][1],  "%.5f" % sjets1[3][
-                    2],  "%.5f" % sjets1[3][3],  "%.5f" % sjets1[3][4],  "%.5f" % sjets1[3][5],
-                "%.5f" % sjets1[4][0],  "%.5f" % sjets1[4][1],  "%.5f" % sjets1[4][
-                    2],  "%.5f" % sjets1[4][3],  "%.5f" % sjets1[4][4],  "%.5f" % sjets1[4][5],
-                "%.5f" % target_W_had[0], "%.5f" % target_W_had[1], "%.5f" % target_W_had[
-                    2], "%.5f" % target_W_had[3], "%.5f" % target_W_had[4],
-                "%.5f" % target_W_lep[0], "%.5f" % target_W_lep[1], "%.5f" % target_W_lep[
-                    2], "%.5f" % target_W_lep[3], "%.5f" % target_W_lep[4],
-                "%.5f" % target_b_had[0], "%.5f" % target_b_had[1], "%.5f" % target_b_had[
-                    2], "%.5f" % target_b_had[3], "%.5f" % target_b_had[4],
-                "%.5f" % target_b_lep[0], "%.5f" % target_b_lep[1], "%.5f" % target_b_lep[
-                    2], "%.5f" % target_b_lep[3], "%.5f" % target_b_lep[4],
-                "%.5f" % target_t_had[0], "%.5f" % target_t_had[1], "%.5f" % target_t_had[
-                    2], "%.5f" % target_t_had[3], "%.5f" % target_t_had[4],
-                "%.5f" % target_t_lep[0], "%.5f" % target_t_lep[1], "%.5f" % target_t_lep[
-                    2], "%.5f" % target_t_lep[3], "%.5f" % target_t_lep[4],
-                "%.5f" % lep_flipped.Pt(),     "%.5f" % lep_flipped.Eta(
-                ),     "%.5f" % lep_flipped.Phi(),
-                "%.5f" % sjets1[0][6],  "%.5f" % sjets1[0][7],  "%.5f" % sjets1[0][8],
-                "%.5f" % sjets1[1][6],  "%.5f" % sjets1[1][7],  "%.5f" % sjets1[1][8],
-                "%.5f" % sjets1[2][6],  "%.5f" % sjets1[2][7],  "%.5f" % sjets1[2][8],
-                "%.5f" % sjets1[3][6],  "%.5f" % sjets1[3][7],  "%.5f" % sjets1[3][8],
-                "%.5f" % sjets1[4][6],  "%.5f" % sjets1[4][7],  "%.5f" % sjets1[4][8],
-                "%.5f" % target_W_had[5], "%.5f" % target_W_had[6], "%.5f" % target_W_had[7],
-                "%.5f" % target_W_lep[5], "%.5f" % target_W_lep[6], "%.5f" % target_W_lep[7],
-                "%.5f" % target_b_had[5], "%.5f" % target_b_had[6], "%.5f" % target_b_had[7],
-                "%.5f" % target_b_lep[5], "%.5f" % target_b_lep[6], "%.5f" % target_b_lep[7],
-                "%.5f" % target_t_had[5], "%.5f" % target_t_had[6], "%.5f" % target_t_had[7],
-                "%.5f" % target_t_lep[5], "%.5f" % target_t_lep[6], "%.5f" % target_t_lep[7],
-                "%.5f" % H_t, "%i" % closest_b, "%.5f" % DeltaPhi, "%.5f" % InvMass
-            ))
-        # Change the angle to rotate by
-        phi = np.random.uniform(- np.pi, np.pi)
+    # PRINT OUT TO FILE
+    csvwriter.writerow((
+        "%i" % runNumber, "%i" % eventNumber, "%.5f" % weight, "%i" % jets_n, "%i" % bjets_n,
+        "%f" % lep.Px(), "%f" % lep.Py(), "%f" % lep.Pz(), "%f" % met_met, "%f" % met_phi,
+        "%f" % jets[0][0], "%f" % jets[0][1], "%f" % jets[0][2], "%f" % jets[0][3], "%f" % jets[0][4], "%f" % jets[0][5],
+        "%f" % jets[1][0], "%f" % jets[1][1], "%f" % jets[1][2], "%f" % jets[1][3], "%f" % jets[1][4], "%f" % jets[1][5],
+        "%f" % jets[2][0], "%f" % jets[2][1], "%f" % jets[2][2], "%f" % jets[2][3], "%f" % jets[2][4], "%f" % jets[2][5],
+        "%f" % jets[3][0], "%f" % jets[3][1], "%f" % jets[3][2], "%f" % jets[3][3], "%f" % jets[3][4], "%f" % jets[3][5],
+        "%f" % jets[4][0], "%f" % jets[4][1], "%f" % jets[4][2], "%f" % jets[4][3], "%f" % jets[4][4], "%f" % jets[4][5],
+        "%f" % t_lep.Px(),  "%f" % t_lep.Py(), "%f" % t_lep.Pz(
+        ), "%f" % t_lep.E(), "%f" % t_lep.M(),
+        "%f" % t_had.Px(),  "%f" % t_had.Py(), "%f" % t_had.Pz(
+        ), "%f" % t_had.E(), "%f" % t_had.M(),
+        "%f" % W_lep.Px(),  "%f" % W_lep.Py(), "%f" % W_lep.Pz(
+        ), "%f" % W_lep.E(), "%f" % W_lep.M(),
+        "%f" % W_had.Px(),  "%f" % W_had.Py(), "%f" % W_had.Pz(
+        ), "%f" % W_had.E(), "%f" % W_had.M(),
+        "%f" % b_lep.Px(),  "%f" % b_lep.Py(), "%f" % b_lep.Pz(
+        ), "%f" % b_lep.E(), "%f" % b_lep.M(),
+        "%f" % b_had.Px(),  "%f" % b_had.Py(), "%f" % b_had.Pz(
+        ), "%f" % b_had.E(), "%f" % b_had.M(),
+        )
 
 
 ##############################################################
 # Close Program
 outfile.close()
 
-f_good = 100. * n_good / n_entries
+f_good=100. * n_good / n_entries
 print("INFO: output file:", outfilename)
 print("INFO: %i entries written (%.2f %%)" % (n_good, f_good))
